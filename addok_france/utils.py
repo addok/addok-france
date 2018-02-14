00f0868af11a59dae831a5bc127fc182a8fea17c
cquest <cquest@openstreetmap.fr>
2018-02-14 23:38:36 +0100

    print() removed

--- addok_france/utils.py
+++ addok_france/utils.py
@@ -51,11 +51,8 @@ NUMBER_PATTERN = re.compile(r'\b\d{1,4}[a-z]?\b', flags=re.IGNORECASE)
 
 def clean_query(q):
     q = re.sub(r'([\d]{5})', r' \1 ', q, flags=re.IGNORECASE)
-    print(q)
     q = re.sub(r'(^| )(b\.?p\.?|cs|tsa|cidex) *(n(o|°|) *|)[\d]+ *', r'\1', q, flags=re.IGNORECASE)
-    print(q)
     q = re.sub(r'([\d]{2})[\d]{3}(.*)c(e|é)dex ?[\d]*', r'\1\2', q, flags=re.IGNORECASE)
-    print(q)
     q = re.sub('c(e|é)dex ?[\d]*', '', q, flags=re.IGNORECASE)
     q = re.sub('\d{,2}(e|[eè]me) ([eé]tage)', '', q, flags=re.IGNORECASE)
     q = re.sub(' {2,}', ' ', q, flags=re.IGNORECASE)
