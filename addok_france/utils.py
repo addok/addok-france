a6af85ac9dd55264f1a9dcd8ef5ef8336ae5ee57
Yohan Boniface <yohanboniface@free.fr>
2022-03-13 16:44:35 +0100

    chore: compile clean regex patterns

--- addok_france/utils.py
+++ addok_france/utils.py
@@ -1,64 +1,114 @@
 import re
 
 TYPES = [
-    'av(enue)?', 'r(ue)?', 'b(oulevar)?d', 'all[ée]es?', 'impasse', 'place',
-    'chemin', 'rocade', 'route', 'l[ôo]tissement', 'mont[ée]e', 'c[ôo]te',
-    'clos', 'champ', 'bois', 'taillis', 'boucle', 'passage', 'domaine',
-    'étang', 'etang', 'quai', 'desserte', 'pré', 'porte', 'square', 'mont',
-    'r[ée]sidence', 'parc', 'cours?', 'promenade', 'hameau', 'faubourg',
-    'ilot', 'berges?', 'via', 'cit[ée]', 'sent(e|ier)', 'rond[- ][Pp]oint',
-    'pas(se)?', 'carrefour', 'traverse', 'giratoire', 'esplanade', 'voie',
-    'chauss[ée]e',
+    "av(enue)?",
+    "r(ue)?",
+    "b(oulevar)?d",
+    "all[ée]es?",
+    "impasse",
+    "place",
+    "chemin",
+    "rocade",
+    "route",
+    "l[ôo]tissement",
+    "mont[ée]e",
+    "c[ôo]te",
+    "clos",
+    "champ",
+    "bois",
+    "taillis",
+    "boucle",
+    "passage",
+    "domaine",
+    "étang",
+    "etang",
+    "quai",
+    "desserte",
+    "pré",
+    "porte",
+    "square",
+    "mont",
+    "r[ée]sidence",
+    "parc",
+    "cours?",
+    "promenade",
+    "hameau",
+    "faubourg",
+    "ilot",
+    "berges?",
+    "via",
+    "cit[ée]",
+    "sent(e|ier)",
+    "rond[- ][Pp]oint",
+    "pas(se)?",
+    "carrefour",
+    "traverse",
+    "giratoire",
+    "esplanade",
+    "voie",
+    "chauss[ée]e",
 ]
-TYPES_REGEX = '|'.join(
-    map(lambda x: '[{}{}]{}'.format(x[0], x[0].upper(), x[1:]), TYPES)
+TYPES_REGEX = "|".join(
+    map(lambda x: "[{}{}]{}".format(x[0], x[0].upper(), x[1:]), TYPES)
 )
-ORDINAL_REGEX = 'bis|ter|quater|quinquies|sexies|[a-z]'
+ORDINAL_REGEX = "bis|ter|quater|quinquies|sexies|[a-z]"
 
 FOLD = {
-    'bis': 'b',
-    'ter': 't',
-    'quater': 'q',
-    'quinquies': 'c',
-    'sexies': 's',
+    "bis": "b",
+    "ter": "t",
+    "quater": "q",
+    "quinquies": "c",
+    "sexies": "s",
 }
 
 # Try to match address pattern when the search sting contains extra info (for
 # example "22 rue des Fleurs 59350 Lille" will be extracted from
 # "XYZ Ets bâtiment B 22 rue des Fleurs 59350 Lille Cedex 23").
 EXTRACT_ADDRESS_PATTERN = re.compile(
-    r'(\b\d{1,4}( *(' + ORDINAL_REGEX + '))?,? +(' + TYPES_REGEX + ') .*(\d{5})?).*',  # noqa
-    flags=re.IGNORECASE)
+    r"(\b\d{1,4}( *("
+    + ORDINAL_REGEX
+    + "))?,? +("
+    + TYPES_REGEX
+    + ") .*(\d{5})?).*",  # noqa
+    flags=re.IGNORECASE,
+)
 
 # Match "bis", "ter", "b", etc.
-ORDINAL_PATTERN = re.compile(r'\b(' + ORDINAL_REGEX + r')\b',
-                             flags=re.IGNORECASE)
+ORDINAL_PATTERN = re.compile(r"\b(" + ORDINAL_REGEX + r")\b", flags=re.IGNORECASE)
 
 # Match "rue", "boulevard", "bd", etc.
-TYPES_PATTERN = re.compile(r'\b(' + TYPES_REGEX + r')\b', flags=re.IGNORECASE)
+TYPES_PATTERN = re.compile(r"\b(" + TYPES_REGEX + r")\b", flags=re.IGNORECASE)
 
 
 # Match number + ordinal, once glued by glue_ordinal (or typed like this in the
 # search string, for example "6bis", "234ter").
-FOLD_PATTERN = re.compile(r'^(\d{1,4})(' + ORDINAL_REGEX + ')$',
-                          flags=re.IGNORECASE)
+FOLD_PATTERN = re.compile(r"^(\d{1,4})(" + ORDINAL_REGEX + ")$", flags=re.IGNORECASE)
 
 
 # Match number once cleaned by glue_ordinal and fold_ordinal (for example
 # "6b", "234t"…)
-NUMBER_PATTERN = re.compile(r'\b\d{1,4}[a-z]?\b', flags=re.IGNORECASE)
+NUMBER_PATTERN = re.compile(r"\b\d{1,4}[a-z]?\b", flags=re.IGNORECASE)
+
+CLEAN_PATTERNS = (
+    (r"([\d]{5})", r" \1 "),
+    (r"(^| )(b\.?p\.?|cs|tsa|cidex) *(n(o|°|) *|)[\d]+ *", r"\1"),
+    (r"([\d]{2})[\d]{3}(.*)c(e|é)dex ?[\d]*", r"\1\2"),
+    (r"c(e|é)dex ?[\d]*", ""),
+    (r"\d{,2}(e|[eè]me) ([eé]tage)", ""),
+    (" {2,}", " "),
+    ("[ -]s/[ -]", " sur "),
+    ("[ -]s/s[ -]", " sous "),
+    ("^lieux?[ -]?dits?\\b(?=.)", ""),
+)
+CLEAN_COMPILED = list(
+    (re.compile(pattern, flags=re.IGNORECASE), replacement)
+    for pattern, replacement in CLEAN_PATTERNS
+)
 
 
 def clean_query(q):
-    q = re.sub(r'([\d]{5})', r' \1 ', q, flags=re.IGNORECASE)
-    q = re.sub(r'(^| )(b\.?p\.?|cs|tsa|cidex) *(n(o|°|) *|)[\d]+ *', r'\1', q, flags=re.IGNORECASE)
-    q = re.sub(r'([\d]{2})[\d]{3}(.*)c(e|é)dex ?[\d]*', r'\1\2', q, flags=re.IGNORECASE)
-    q = re.sub('c(e|é)dex ?[\d]*', '', q, flags=re.IGNORECASE)
-    q = re.sub('\d{,2}(e|[eè]me) ([eé]tage)', '', q, flags=re.IGNORECASE)
-    q = re.sub(' {2,}', ' ', q, flags=re.IGNORECASE)
-    q = re.sub('[ -]s/[ -]', ' sur ', q, flags=re.IGNORECASE)
-    q = re.sub('[ -]s/s[ -]', ' sous ', q, flags=re.IGNORECASE)
-    q = re.sub('^lieux?[ -]?dits?\\b(?=.)', '', q, flags=re.IGNORECASE)
+    for pattern, repl in CLEAN_COMPILED:
+        q = pattern.sub(repl, q)
     q = q.strip()
     return q
 
@@ -92,11 +142,12 @@ def glue_ordinal(tokens):
             continue
         if previous is not None:
             # Matches "bis" either followed by a type or nothing.
-            if (ORDINAL_PATTERN.match(token) and
-                    (not next_ or TYPES_PATTERN.match(next_))):
-                raw = '{} {}'.format(previous, token)
+            if ORDINAL_PATTERN.match(token) and (
+                not next_ or TYPES_PATTERN.match(next_)
+            ):
+                raw = "{} {}".format(previous, token)
                 # Space removed to maximize chances to get a hit.
-                token = previous.update(raw.replace(' ', ''), raw=raw)
+                token = previous.update(raw.replace(" ", ""), raw=raw)
             else:
                 # False positive.
                 yield previous
@@ -108,9 +159,12 @@ def flag_housenumber(tokens):
     # Only keep first match (avoid noise in the middle of the search query).
     found = False
     for previous, token, next_ in neighborhood(tokens):
-        if ((token.is_first or (next_ and TYPES_PATTERN.match(next_)))
-                and NUMBER_PATTERN.match(token) and not found):
-            token.kind = 'housenumber'
+        if (
+            (token.is_first or (next_ and TYPES_PATTERN.match(next_)))
+            and NUMBER_PATTERN.match(token)
+            and not found
+        ):
+            token.kind = "housenumber"
             found = True
         yield token
 
@@ -123,8 +177,7 @@ def fold_ordinal(s):
         except (IndexError, ValueError):
             pass
         else:
-            s = s.update('{}{}'.format(number,
-                                       FOLD.get(ordinal.lower(), ordinal)))
+            s = s.update("{}{}".format(number, FOLD.get(ordinal.lower(), ordinal)))
     return s
 
 
@@ -137,31 +190,31 @@ def remove_leading_zeros(s):
 def make_labels(helper, result):
     if result.labels:
         return
-    housenumber = getattr(result, 'housenumber', None)
+    housenumber = getattr(result, "housenumber", None)
 
     def add(labels, label):
         labels.insert(0, label)
         if housenumber:
-            label = '{} {}'.format(housenumber, label)
+            label = "{} {}".format(housenumber, label)
             labels.insert(0, label)
 
     city = result.city
     postcode = result.postcode
-    names = result._rawattr('name')
+    names = result._rawattr("name")
     if not isinstance(names, (list, tuple)):
         names = [names]
     for name in names:
         labels = []
         label = name
-        if postcode and result.type == 'municipality':
-            add(labels, '{} {}'.format(label, postcode))
-            add(labels, '{} {}'.format(postcode, label))
+        if postcode and result.type == "municipality":
+            add(labels, "{} {}".format(label, postcode))
+            add(labels, "{} {}".format(postcode, label))
         add(labels, label)
         if city and city != label:
-            add(labels, '{} {}'.format(label, city))
+            add(labels, "{} {}".format(label, city))
             if postcode:
-                label = '{} {}'.format(label, postcode)
+                label = "{} {}".format(label, postcode)
                 add(labels, label)
-                label = '{} {}'.format(label, city)
+                label = "{} {}".format(label, city)
                 add(labels, label)
         result.labels.extend(labels)
